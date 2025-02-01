64e0e9ab9bf230e2873d145695544eabd34365fb
Niklas Röttger <niklas.roettger@outlook.de>
2025-02-01 12:14:51 +0100

    add usehookform and zod for form validation

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,29 +1,51 @@
-import { TodoFromForm } from "../../../types/todo";
 import styles from "./Form.module.css";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-export default function Form({
-  onAddTodo,
-}: {
+const todoSchema = z.object({
+  title: z.string().min(3, "Der Titel muss mindestens 3 Zeichen lang sein"),
+  column: z.enum([
+    "Backlog",
+    "Montag",
+    "Dienstag",
+    "Mittwoch",
+    "Donnerstag",
+    "Freitag",
+    "Samstag",
+    "Sonntag",
+  ]),
+  notes: z.string().optional(),
+});
+
+type TodoFromForm = z.infer<typeof todoSchema>;
+
+interface FormProps {
   onAddTodo: (newTodo: TodoFromForm) => void;
-}) {
-  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
+}
 
-    const form = event.currentTarget;
-
-    const formData = new FormData(form as HTMLFormElement);
-    const data = Object.fromEntries(formData) as TodoFromForm;
-
+export default function Form({ onAddTodo }: FormProps) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<TodoFromForm>({
+    resolver: zodResolver(todoSchema),
+  });
+  function onSubmit(data: TodoFromForm) {
     onAddTodo(data);
+    reset();
   }
 
   return (
-    <form onSubmit={handleSubmit} className={styles.form}>
+    <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
       <label htmlFor="title">Titel*</label>
-      <input name="title" id="title" type="text" required aria-required />
+      <input id="title" type="text" required {...register("title")} />
+      {errors.title && <p className={styles.error}>{errors.title.message}</p>}
       <label>
         Wochentag*
-        <select required aria-required name="column">
+        <select required {...register("column")}>
           <option value="Backlog">Backlog</option>
           <option value="Montag">Montag</option>
           <option value="Dienstag">Dienstag</option>
@@ -34,8 +56,11 @@ export default function Form({
           <option value="Sonntag">Sonntag</option>
         </select>
       </label>
+      {errors.column && (
+        <p className={styles.error}>Bitte einen Wochentag auswählen</p>
+      )}
       <label htmlFor="notes">weitere Notizen</label>
-      <textarea rows={5} name="notes" id="notes"></textarea>
+      <textarea rows={5} {...register("notes")} id="notes"></textarea>
       <button
         type="submit"
         className={styles.button}
