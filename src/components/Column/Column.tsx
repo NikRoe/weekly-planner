f154614802d39be0bd12ab80d1b375e823e631d6
Niklas Röttger <niklas.roettger@outlook.de>
2025-02-02 12:18:38 +0100

    add dndkit

--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -3,6 +3,10 @@ import { TodoList, TodoFromForm, Todo } from "../../../types/todo";
 import styles from "./Column.module.css";
 import Modal from "../Modal/Modal";
 import Form from "../Form/Form";
+import { useDroppable } from "@dnd-kit/core";
+import { SortableContext } from "@dnd-kit/sortable";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
 interface ColumnProps {
   name: string;
@@ -19,6 +23,9 @@ export default function Column({
 }: ColumnProps) {
   const [todoToEdit, setTodoToEdit] = useState<Todo>();
   const [isOpen, setIsOpen] = useState(false);
+  const { setNodeRef } = useDroppable({
+    id: name,
+  });
 
   function handleToggleModal() {
     setIsOpen(!isOpen);
@@ -37,42 +44,47 @@ export default function Column({
 
   return (
     <>
-      <ul className={styles.list}>
+      <ul className={styles.list} ref={setNodeRef}>
         <h2 className={styles.title}>{name}</h2>
-        {todos.map((todo) => (
-          <li
-            key={todo.id}
-            className={styles.card}
-            onClick={() => {
-              handleToggleModal();
-              setTodoToEdit(todo);
-            }}
-          >
-            <button
-              onClick={(event) => {
-                event.stopPropagation();
-                handleUpdateStatus(todo);
+        <SortableContext items={todos.map((todo) => todo.id)}>
+          {todos.map((todo) => (
+            <SortableItem
+              key={todo.id}
+              todo={todo}
+              onClick={() => {
+                handleToggleModal();
+                setTodoToEdit(todo);
               }}
-              type="button"
-              className={`${styles.button}   ${
-                todo.status === "Done" ? styles.done : ""
-              }`}
-            ></button>
-            {todo.title}
-            <button
-              type="button"
-              onClick={(event) => {
-                event.stopPropagation();
-                onDeleteTodo(todo.id);
-              }}
-              aria-label="Eintrag löschen"
-              className={styles.deleteButton}
             >
-              X
-            </button>
-          </li>
-        ))}
+              <>
+                <button
+                  onClick={(event) => {
+                    event.stopPropagation();
+                    handleUpdateStatus(todo);
+                  }}
+                  type="button"
+                  className={`${styles.button}   ${
+                    todo.status === "Done" ? styles.done : ""
+                  }`}
+                ></button>
+                {todo.title}
+                <button
+                  type="button"
+                  onClick={(event) => {
+                    event.stopPropagation();
+                    onDeleteTodo(todo.id);
+                  }}
+                  aria-label="Eintrag löschen"
+                  className={styles.deleteButton}
+                >
+                  X
+                </button>
+              </>
+            </SortableItem>
+          ))}
+        </SortableContext>
       </ul>
+
       {isOpen && (
         <Modal onClose={handleToggleModal}>
           <Form
@@ -85,5 +97,36 @@ export default function Column({
         </Modal>
       )}
     </>
+  );
+}
+
+function SortableItem({
+  todo,
+  children,
+  onClick,
+}: {
+  todo: Todo;
+  children: JSX.Element;
+  onClick: () => void;
+}) {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: todo.id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
+  return (
+    <li
+      onClick={onClick}
+      className={styles.card}
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+    >
+      {children}
+    </li>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,28 +5,36 @@ import Form from "@/components/Form/Form";
 import Column from "@/components/Column/Column";
 import styles from "@/styles/Home.module.css";
 import Modal from "@/components/Modal/Modal";
+import {
+  DndContext,
+  DragEndEvent,
+  closestCorners,
+  useSensor,
+  useSensors,
+  PointerSensor,
+} from "@dnd-kit/core";
 
 const initialTodos: TodoList = [
   {
-    id: "1",
+    id: "12",
     title: "Briefe",
     column: "Donnerstag",
     status: "Done",
   },
   {
-    id: "2",
+    id: "23",
     title: "Saugen",
     column: "Donnerstag",
     status: "Open",
   },
   {
-    id: "3",
+    id: "34",
     title: "Splid",
     column: "Freitag",
     status: "Done",
   },
   {
-    id: "4",
+    id: "214",
     title: "Splid",
     column: "Samstag",
     status: "Open",
@@ -48,6 +56,15 @@ export default function Home() {
   const [todos, setTodos] = useState<TodoList>(initialTodos);
   const [isOpen, setIsOpen] = useState(false);
 
+  const pointerSensor = useSensor(PointerSensor, {
+    activationConstraint: {
+      delay: 100,
+      tolerance: 5,
+    },
+  });
+
+  const sensors = useSensors(pointerSensor);
+
   function handleAddTodo(newTodo: TodoFromForm) {
     const todo = { ...newTodo, status: "Open", id: nanoid() };
     setTodos([...todos, todo]);
@@ -67,23 +84,54 @@ export default function Home() {
     setTodos(todos.filter((todo) => todo.id !== idToDelete));
   }
 
+  function handleDragEnd(event: DragEndEvent) {
+    const { over, active, collisions } = event;
+
+    if (!over) return;
+
+    const filteredCollisions = collisions?.filter((collision) =>
+      columnNames.some((name) => name === collision.id)
+    );
+
+    if (!filteredCollisions) return;
+
+    const { id: activeId } = active;
+
+    // @ts-ignore
+    setTodos((prevTodos: TodoList) => {
+      return prevTodos.map((todo) =>
+        todo.id === activeId
+          ? { ...todo, column: filteredCollisions[0].id }
+          : todo
+      );
+    });
+  }
+
   return (
     <>
       <h1 style={{ textAlign: "center", margin: "1rem" }}>Weekly Planner</h1>
-      <div className={styles.columnWrapper}>
-        {columnNames.map((column, index) => {
-          const filteredTodos = todos.filter((todo) => todo.column === column);
-          return (
-            <Column
-              key={index}
-              name={column}
-              todos={filteredTodos}
-              onEditTodo={handleEditTodo}
-              onDeleteTodo={handleDeleteTodo}
-            />
-          );
-        })}
-      </div>
+      <DndContext
+        collisionDetection={closestCorners}
+        onDragEnd={handleDragEnd}
+        sensors={sensors}
+      >
+        <div className={styles.columnWrapper}>
+          {columnNames.map((column, index) => {
+            const filteredTodos = todos.filter(
+              (todo) => todo.column === column
+            );
+            return (
+              <Column
+                key={index}
+                name={column}
+                todos={filteredTodos}
+                onEditTodo={handleEditTodo}
+                onDeleteTodo={handleDeleteTodo}
+              />
+            );
+          })}
+        </div>
+      </DndContext>
 
       <button
         type="button"
