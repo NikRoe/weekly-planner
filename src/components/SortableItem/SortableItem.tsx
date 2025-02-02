900193b6efb31f988b26bc95c38f25fa3f7c3215
Niklas Röttger <niklas.roettger@outlook.de>
2025-02-02 12:32:27 +0100

    add interface for sortableitem

--- src/components/SortableItem/SortableItem.tsx
+++ src/components/SortableItem/SortableItem.tsx
@@ -3,15 +3,17 @@ import { CSS } from "@dnd-kit/utilities";
 import { Todo } from "../../../types/todo";
 import styles from "../Column/Column.module.css";
 
+interface SortableItemProps {
+  todo: Todo;
+  children: JSX.Element;
+  onClick: () => void;
+}
+
 export default function SortableItem({
   todo,
   children,
   onClick,
-}: {
-  todo: Todo;
-  children: JSX.Element;
-  onClick: () => void;
-}) {
+}: SortableItemProps) {
   const { attributes, listeners, setNodeRef, transform, transition } =
     useSortable({ id: todo.id });
 
