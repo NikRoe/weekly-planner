62099350d15be974a0e14d4cc3048bea8749748d
Niklas Röttger <niklas.roettger@outlook.de>
2025-02-03 15:29:07 +0100

    adjust loading spinner text position

--- src/components/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -4,7 +4,7 @@ export default function LoadingSpinner() {
   return (
     <div className={styles.spinnerContainer}>
       <div className={styles.spinner}></div>
-      <div className={styles.spinnerText}>Loading...</div>
+      <div className={styles.spinnerText}>Loading</div>
     </div>
   );
 }
