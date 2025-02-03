12e398f4627a4cfa370f62aa0d68d84359ddf9cd
Niklas Röttger <niklas.roettger@outlook.de>
2025-02-03 14:49:01 +0100

    add optimistic ui for drag and drop

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,17 +17,22 @@ import {
   handleAddTodo,
   handleDeleteTodo,
   handleEditTodo,
-  handleDragEnd,
 } from "@/services/todos";
 import LoadingSpinner from "@/components/LoadingSpinner/LoadingSpinner";
+import { DragEndEvent } from "@dnd-kit/core";
 
 export default function Home() {
-  const { data: todos, isLoading, error } = useSWR<TodoList>("/api/todos");
+  const {
+    data: todos,
+    isLoading,
+    error,
+    mutate,
+  } = useSWR<TodoList>("/api/todos");
   const [isOpen, setIsOpen] = useState(false);
 
   const pointerSensor = useSensor(PointerSensor, {
     activationConstraint: {
-      delay: 100,
+      delay: 50,
       tolerance: 5,
     },
   });
@@ -42,24 +47,60 @@ export default function Home() {
   if (error) return <div>An Error Occurred</div>;
   if (!todos) return;
 
+  function optimisticHandleDragEnd(event: DragEndEvent) {
+    const { over, active, collisions } = event;
+
+    if (!over) return;
+
+    const filteredCollisions = collisions?.filter((collision) =>
+      columnNames.includes(collision.id as string)
+    );
+
+    if (!filteredCollisions?.length) return;
+
+    const activeId = active.id;
+    const newColumn = filteredCollisions[0].id as string;
+    const optimisticTodos = todos?.map((todo) =>
+      todo.id === activeId ? { ...todo, column: newColumn } : todo
+    );
+
+    mutate(
+      async () => {
+        await fetch(`/api/todos/${activeId}`, {
+          method: "PATCH",
+          body: JSON.stringify({ column: newColumn }),
+          headers: { "Content-Type": "application/json" },
+        });
+
+        return optimisticTodos;
+      },
+      {
+        optimisticData: optimisticTodos,
+        populateCache: true,
+        revalidate: false,
+        rollbackOnError: true,
+      }
+    );
+  }
+
   return (
     <>
       <h1 style={{ textAlign: "center", margin: "1rem" }}>Weekly Planner</h1>
       <DndContext
         collisionDetection={closestCorners}
-        onDragEnd={handleDragEnd}
+        onDragEnd={optimisticHandleDragEnd}
         sensors={sensors}
       >
         <div className={styles.columnWrapper}>
           {columnNames.map((column, index) => {
-            const filteredTodos = todos.filter(
+            const filteredTodos = todos?.filter(
               (todo) => todo.column === column
             );
             return (
               <Column
                 key={index}
                 name={column}
-                todos={filteredTodos}
+                todos={filteredTodos || []}
                 onEditTodo={handleEditTodo}
                 onDeleteTodo={handleDeleteTodo}
               />
--- src/services/todos.ts
+++ src/services/todos.ts
@@ -1,6 +1,4 @@
 import { Todo, TodoFromForm } from "../../types/todo";
-import { DragEndEvent } from "@dnd-kit/core";
-import { columnNames } from "@/utils/todos";
 import { mutate } from "swr";
 
 export async function handleAddTodo(newTodo: TodoFromForm) {
@@ -28,29 +26,6 @@ export async function handleEditTodo(updatedTodo: Todo) {
   const response = await fetch(`/api/todos/${updatedTodo.id}`, {
     method: "PUT",
     body: JSON.stringify(updatedTodo),
-    headers: { "Content-Type": "application/json" },
-  });
-  if (response.ok) {
-    mutate("/api/todos");
-  }
-}
-
-export async function handleDragEnd(event: DragEndEvent) {
-  const { over, active, collisions } = event;
-
-  if (!over) return;
-
-  const filteredCollisions = collisions?.filter((collision) =>
-    columnNames.includes(collision.id as string)
-  );
-
-  if (!filteredCollisions?.length) return;
-
-  const activeId = active.id;
-
-  const response = await fetch(`/api/todos/${activeId}`, {
-    method: "PATCH",
-    body: JSON.stringify({ column: filteredCollisions[0].id as string }),
     headers: { "Content-Type": "application/json" },
   });
   if (response.ok) {
